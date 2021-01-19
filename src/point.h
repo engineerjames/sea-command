48a9c94d74b6bca5a6ed76ce2a584d065caf160d
James Armes <jamesleearmes@gmail.com>
2021-01-18 22:54:10 -0700

    Add basic point implementation

---
+++ src/point.h
@@ -0,0 +1,28 @@
+#ifndef POINT_H
+#define POINT_H
+
+#include <cmath>
+
+template < typename T >
+struct Point
+{
+    T x;
+    T y;
+
+    Point( T x, T y )
+        : x( x )
+        , y( y )
+    {
+    }
+
+    static double DistanceBetween( Point< T > p1, Point< T > p2 )
+    {
+        double deltaX   = std::pow( p2.x - p1.x, 2.0 );
+        double deltaY   = std::pow( p2.y - p1.y, 2.0 );
+        double distance = std::sqrt( deltaX + deltaY );
+
+        return distance;
+    }
+};
+
+#endif
