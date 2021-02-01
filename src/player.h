72b8c6a68de68d84fb5244ba5e8c86957225ae8c
James Armes <jamesleearmes@gmail.com>
2021-01-31 19:24:12 -0700

    Add template player

---
+++ src/player.h
@@ -0,0 +1,21 @@
+#include <array>
+
+#include "ship.h"
+
+// class PlacementStrategy;
+// class ShotStrategy;
+
+class Player
+{
+   public:
+    // Player(const PlacementStrategy& placement, const ShotStrategy& shot);
+
+    static const int N_SHIPS = 5;
+
+   private:
+    Ship< 5 > carrier_;
+    Ship< 4 > battleship_;
+    Ship< 3 > destroyer_;
+    Ship< 3 > submarine_;
+    Ship< 2 > patrolBoat_;
+};
